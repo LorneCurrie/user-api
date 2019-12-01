5fd50ae3ea338122409c8ed88cde556f6f71035f
Lorne Currie <lcurrie@todd-digital.com>
2019-12-01 21:06:43 +1300

    Deleted Database folder and moved API to the project root

--- database/mysql/setup.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'devDbUser'@'%' IDENTIFIED BY 'thisAmazingPassword';
-GRANT USAGE ON *.* TO 'devDbUser'@'%' IDENTIFIED BY 'thisAmazingPassword';
-GRANT ALL PRIVILEGES ON *.* TO 'devDbUser'@'%';
---
+++ env.dev.sh
@@ -0,0 +1,6 @@
+export MYSQL_DB_HOST=127.0.0.1
+export MYSQL_DB_USER=devDbUser
+export MYSQL_DB_PASSWORD=thisAmazingPassword
+export MYSQL_DB_DATABASE=api_dev
+export LOG_LEVEL=debug
+export NODE_ENV=development
